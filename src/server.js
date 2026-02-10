c8b3543346de8f79054634d1f50fd83062c35c01
sujal0786 <sujal6067@gmail.com>
2026-02-10 12:39:58 +0530

    add

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
+// require("dotenv").config();
+// const app = require("./app");
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
 require("dotenv").config();
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+module.exports = app;
